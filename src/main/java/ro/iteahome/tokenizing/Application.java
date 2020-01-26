a539376365f40f6a9e5f31d4b25166d1ab31c544
gneamt <georgeneamt2004@yahoo.com>
2020-01-26 02:03:18 +0200

    add tokenizer

--- src/main/java/ro/iteahome/tokenizing/Application.java
+++ src/main/java/ro/iteahome/tokenizing/Application.java
@@ -10,10 +10,23 @@ Do tokenize a such string and create the Student object with the data found ther
 
 import ro.iteahome.tokenizing.model.Student;
 
+import java.util.StringTokenizer;
+
 public class Application {
     public static void main(String[] args){
 
         Student s1 = new Student(1, "Ionut", "Popescu", "Alba-Iulia", "Romana", 10);
         System.out.println(s1.toString());
+
+        String studentString = "2,George,Neamt,Cluj-Napoca,Romana,9";
+        Student s2 = createStudentUsingTokenizer(studentString);
+        System.out.println(s2.toString());
     }
+
+    private static Student createStudentUsingTokenizer(String inputString){
+
+        String[] tokenized = inputString.split(",");
+        Student student = new Student(Integer.parseInt(tokenized[0]), tokenized[1], tokenized[2], tokenized[3], tokenized[4], Integer.parseInt(tokenized[5]));
+        return student;
+    };
 }
